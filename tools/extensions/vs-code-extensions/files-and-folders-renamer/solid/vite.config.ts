7aa6dee8e1b71abfbe6fddd234806cc527ab13ca
Tane Kim <tanekim77@hotmail.com>
2021-09-03 04:39:56 -0400

    b

---
+++ tools/extensions/vs-code-extensions/files-and-folders-renamer/solid/vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "vite";
+import solidPlugin from "vite-plugin-solid";
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
+
+export default defineConfig({
+  plugins: [solidPlugin(),vanillaExtractPlugin()],
+  build: {
+    target: "esnext",
+    polyfillDynamicImport: false,
+  },
+});
