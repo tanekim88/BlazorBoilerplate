a8ce41e4bb02454ae1bd29a726ab9285cb0001ce
Tane Kim <tanekim77@hotmail.com>
2021-09-06 06:26:13 -0400

    b

--- Js/apps/shared/webpack.prod.ts
+++
@@ -1,8 +0,0 @@
-import { WebpackProdService } from './src/webpack/webpack-prod/webpack-prod.service';
-import { WebpackModule } from './src/webpack/webpack.module';
-import { WebpackBase } from './webpack.base';
-const webpackBase = new WebpackBase(WebpackModule, [WebpackProdService]);
-
-export default webpackBase.createWebpackConfigs;
-
-webpackBase.execute();
--- tools/extensions/vs-code-extensions/files-and-folders-renamer/svelte/src/routes/options/index.svelte
+++
@@ -1,3 +0,0 @@
-<div>
-	Options
-</div>
