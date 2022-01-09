d9e3cd32dce8f36748dc16896fe82e9e031132d5
Tane Kim <tanekim77@hotmail.com>
2022-01-09 06:16:07 -0500

    sdf

--- languages/js/apps/auth/src/vite/vite-plugins/vite-plugins.service.ts
+++ languages/js/apps/auth/src/vite/vite-plugins/vite-plugins.service.ts
@@ -21,10 +21,40 @@ export class AuthVitePluginsService extends VitePluginsService {
         return [
             solidPlugin(),
             ...this.vitePluginGlobInputService.createManyPlugins({
+                externalsForHtml: [
+                    // {
+                    //     html: `
+                    //     <link href="_content/Material.Blazor/material.blazor.min.css" rel="stylesheet" />
+                    //     <link href="_content/Material.Blazor/material-components-web.min.css" rel="stylesheet" />
+                    // `, insertAt: '@head-start'
+                    // },
+                    // { html: '<link href="BlazorApp.Client.styles.css" rel="stylesheet" />', insertAt: '@head-end' },
+                    // {
+                    //     html: `
+                    // <script src="_content/Material.Blazor/material.blazor.min.js"></script>
+                    // <script>
+                    //     MaterialBlazor.MBTooltip.numbers.HIDE_DELAY_MS = 0
+                    // </script>
+                    // <script src="_content/Microsoft.AspNetCore.Components.WebAssembly.Authentication/AuthenticationService.js"></script>
+                    // <script src="_framework/blazor.webassembly.js"></script>
+                    // <script>navigator.serviceWorker.register("[SERVICEWORKER_PATH]", {type:"module"});</script>
+                    // `, insertAt: '@body-end'
+                    // },
+                ],
+                sass: [
+                    {
+                        fromPath: AuthPaths.Pages.toAbsolutePath()
+                    },
+                ],
                 inputs: [
                     {
                         fromPath: authPaths.src.templates['_Layout.cshtml'].toAbsolutePath(),
                         toRelativePath: path.join(AuthPaths.Pages.Shared.toRelativePath(AuthPaths.toAbsolutePath()), '_Layout.cshtml')
+                    },
+                    {
+                        fromPath: this.authEnvironmentService.localPaths.src['service-worker']['index.ts'].toAbsolutePath(),
+                        htmlToken: '[SERVICEWORKER_PATH]',
+                        toRelativePath: path.join(AuthPaths.wwwroot.toRelativePath(AuthPaths.toAbsolutePath()), 'service-worker.[hash].js')
                     },
                 ],
                 copy: [
